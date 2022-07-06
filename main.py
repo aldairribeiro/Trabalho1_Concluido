72b12f794057cf01bce3d040350cb83016afb69e
aldairribeiro <aldair.martis0@gmail.com>
2022-07-06 10:41:33 -0300

    atualizado o  exercicio

--- main.py
+++ main.py
@@ -3,20 +3,29 @@
  #Apresenta na tela estas informações de modo que permaneçam alinhados verticalmente com formatação de interpolação.
 
 
-    
-nome_1 = input ("Digite o nome da Primeira Pessoa:")
-idade_1 = input ("Digite a idade da Primeira Pessoa:")
-peso_1 = input ("Digite o peso da Primeira Pessoa:")
-altura_1 = input ("Digite a altura da Primeira Pessoa:")
+nome_1 = input ("Digite seu nome:")
+print ("seu nome e: ", nome_1)
+idade_1 = input ("Digite sua idade:")
+print ("Sua Idade e: ", idade_1,'anos')
+peso_1 = input ("Digite seu peso:")
+print ("seu Peso e: ", peso_1, 'kilos')
+altura_1 = input ("Digite sua altura:")
+print ("Sua Altura e: ", altura_1, 'cm')
 
-nome_2 = input ("Digite o nome da Segunda Pessoa:")
-idade_2 = input ("Digite a idade da Segunda Pessoa:")
-peso_2 = input ("Digite o peso da Segunda Pessoa:")
-altura_2 = input ("Digite a altura da Segunda Pessoa:")
+nome_2 = input ("Digite seu nome:")
+print ("seu nome e: ", nome_2)
+idade_2 = input ("Digite sua idade:")
+print ("Sua Idade e: ", idade_2, 'anos')
+peso_2 = input ("Digite seu peso:")
+print ("seu Peso e: ", peso_2,'kilos')
+altura_2 = input ("Digite sua altura:")
+print ("Sua Altura e: ", altura_2,'cm')
 
-nome_3 = input ("Digite o nome da Terceira Pessoa:")
-idade_3 = input ("Digite a idade da Terceira Pessoa:")
-peso_3 = input ("Digite o peso da Terceira Pessoa:")
-altura_3 = input ("Digite a altura da Terceira Pessoa:")
-     
-
+nome_3= input ("Digite seu nome:")
+print ("seu nome e: ", nome_3)
+idade_3 = input ("Digite sua idade:")
+print ("Sua Idade e: ", idade_3, 'anos')
+peso_3 = input ("Digite seu peso:")
+print ("seu Peso e: ", peso_3, ' kilos')
+altura_3 = input ("Digite sua altura:")
+print ("Sua Altura e: ",altura_3,'cm')
